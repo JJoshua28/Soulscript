bd894498bd14e5ccbbca0cfacaea7b006349b192
Joshua <j.joshua34@outlook.com>
2024-06-12 14:23:42 +0100

    blocking type updates for gratitude entries

--- src/handlers/entries/gratitude/updateGratitudeEntry.ts
+++ src/handlers/entries/gratitude/updateGratitudeEntry.ts
@@ -11,7 +11,7 @@ const handleUpdateGratitudeEntry = async (req: Request): Promise<Entry> => {
     const entryService = new MongoDBService(entryModel, EntryTypes.GRATITUDE);
     const updateGratitudeUseCase = new UpdateEntryUseCase(entryService);
     
-    if(!req?.body?.update || Object.keys(req?.body?.update).length === 0|| !req?.body?.id) throw new Error(CustomMoodErrors.INVALID_REQUEST);
+    if(!req?.body?.update || Object.keys(req?.body?.update).length === 0|| !req?.body?.id || req?.body?.update?.type) throw new Error(CustomMoodErrors.INVALID_REQUEST);
     if(req?.body?.update?.content && !Array.isArray(req?.body?.update?.content) || req?.body?.update?.content?.length < 1) throw new Error(CustomMoodErrors.INVALID_REQUEST);
     if (req?.body?.update?.datetime && !validDate(req?.body?.update?.datetime)) throw new Error(CustomMoodErrors.INVALID_DATE);
     
